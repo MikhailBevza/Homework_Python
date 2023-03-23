e442861bec95ef06ad8b12330a121acb65980606
Mikhail <miha.bevza@gmail.com>
2023-03-23 15:48:44 +0300

    Третья задача решена

--- Homework1.py
+++ Homework1.py
@@ -23,3 +23,25 @@ if num1 % 6 == 0:
 else:
     print('Задача не имеет решения')
 """
+
+# Задача 6: Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером. Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех. Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
+
+num1 = int(input('Введите номер билета: '))
+
+if num1 > 99999 and num1 < 1000000:
+    first_part_ticket = num1 // 1000
+    second_part_ticket = num1 % 1000
+    first_sum = 0
+    second_sum = 0
+    while first_part_ticket != 0:
+        first_sum += first_part_ticket % 10
+        first_part_ticket //= 10
+    while second_part_ticket != 0:
+        second_sum += second_part_ticket % 10
+        second_part_ticket //= 10
+    if first_sum == second_sum:
+        print('Билет счастливый!')
+    else:
+        print('Билет несчастливый')
+else:
+    print('Номер билета некорректный')
